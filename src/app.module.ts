4b0535d9c510b25d8b5812d26e499f6fafc2f5cf
fernandocgomez <fernandocgomez@live.com>
2024-03-15 00:04:39 -0500

    config env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TodosModule],
+  imports: [ConfigModule.forRoot(), TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
