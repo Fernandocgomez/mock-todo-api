0265b8076da01bcf1c0b5502697778291d217396
fernandocgomez <fernandocgomez@live.com>
2024-03-18 21:35:28 -0500

    done with impl

--- src/todos/todos.services.ts
+++ src/todos/todos.services.ts
@@ -2,18 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { Todo } from './interfaces/todo.interface';
 import { CreateTodoDto } from './dtos/request/create-todo.dto';
 import { TodosRepository } from './todos.repository';
+import { GetAllTodosDto } from './dtos/response/get-all-todos.dto';
 
 @Injectable()
 export class TodosService {
-  constructor(
-    private readonly todosRepository: TodosRepository,
-  ) {}
+  constructor(private readonly todosRepository: TodosRepository) {}
 
   async create(createTodo: CreateTodoDto): Promise<Todo> {
-    return this.todosRepository.create(createTodo);
+    const createdTodo = await this.todosRepository.create(createTodo);
+    const { _id, title, content, isComplete } = createdTodo;
+
+    return { id: _id, title, content, isComplete } as Todo;
   }
 
-  async getAllTodos(): Promise<Todo[]> {
-    return this.todosRepository.getAllTodos();
+  async getAllTodos(): Promise<GetAllTodosDto> {
+    const doneTodos = [];
+    const pendingTodos = [];
+
+    const todos = await this.todosRepository.getAllTodos();
+
+    todos.forEach((todo) => {
+      const { _id, title, content, isComplete } = todo;
+      const systemTodo = { id: _id, title, content, isComplete };
+
+      if (isComplete) {
+        doneTodos.push(systemTodo);
+      } else {
+        pendingTodos.push(systemTodo);
+      }
+    });
+
+    return { doneTodos, pendingTodos };
   }
 }
